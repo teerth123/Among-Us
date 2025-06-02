f015d9154e93b85c3a3352217379aafa33522fd5
DexT_17 Teerth <teerth.kulkarni@walchandsangli.ac.in>
2025-06-02 16:03:32 +0530

    polling logic

--- backend practice/src/server.ts
+++ backend practice/src/server.ts
@@ -33,7 +33,7 @@ io.on("connection", (socket)=>{
 
 })
 
-server.listen(3000, ()=>{
+server.listen(3000,  ()=>{
     console.log("server running on 3000")
 })
 
--- backend practice/src/socket/index.ts
+++ backend practice/src/socket/index.ts
@@ -44,40 +44,57 @@ export function kill(socket: Socket, io: Server) {
             nearestPlayer.dead = true;
 
             // Update room.players
-            const playerIndex = room.players.findIndex((p :player)=> p.username === nearestPlayer!.username);
+            const playerIndex = room.players.findIndex((p: player) => p.username === nearestPlayer!.username);
             if (playerIndex !== -1) room.players[playerIndex] = nearestPlayer;
+
+            const aliveAfterKill = room.players.filter((p:player) => !p.dead);
+            const impostersAlive = aliveAfterKill.filter((p:player)=> p.role === 'imposter').length;
+            const nonImpostersAlive = aliveAfterKill.filter((p:player)=> p.role !== 'imposter').length;
+
+            if (impostersAlive === 0) {
+                io.to(currentPlayer.roomID).emit("endGame", "Crewmates win! All imposters eliminated.");
+                return;
+            }
+
+            if (nonImpostersAlive === 0) {
+                io.to(currentPlayer.roomID).emit("endGame", "Imposters win! No crewmates left.");
+                return;
+            }
+
 
             // Broadcast the kill
             io.to(currentPlayer.roomID).emit("player-killed", {
                 killer: currentPlayer.username,
                 victim: nearestPlayer.username,
             });
+
+
         });
     });
 }
 
 
-export function movement(socket:Socket, io:Server){
-    socket.on("movement", (data:player[])=>{
-        requirePlayerAndRoom(socket, ({player:currentPlayer, room})=>{
+export function movement(socket: Socket, io: Server) {
+    socket.on("movement", (data: player[]) => {
+        requirePlayerAndRoom(socket, ({ player: currentPlayer, room }) => {
 
             //suppose data is an array of positions of all players in a room
-            const playersInRoom:player[] = room.players;
-            
-            if(currentPlayer.dead){
+            const playersInRoom: player[] = room.players;
+
+            if (currentPlayer.dead) {
                 socket.emit("error", "dead players cannot play")
                 return
             }
 
-            for(let p of data){
-                const presentPlayer = playersInRoom.find(x=>x.username===p.username)
-                if(presentPlayer && !presentPlayer.dead){
+            for (let p of data) {
+                const presentPlayer = playersInRoom.find(x => x.username === p.username)
+                if (presentPlayer && !presentPlayer.dead) {
                     presentPlayer.position.x = p.position.x
                     presentPlayer.position.y = p.position.y
                 }
             }
 
-            const alivePlayers:player[] = playersInRoom.filter((p:player)=>!p.dead)
+            const alivePlayers: player[] = playersInRoom.filter((p: player) => !p.dead)
 
             io.to(currentPlayer.roomID).emit("movement", alivePlayers)
         })
@@ -85,30 +102,52 @@ export function movement(socket:Socket, io:Server){
 }
 
 
-export function polling(socket:Socket, io:Server){
+export function polling(socket: Socket, io: Server) {
 
-    socket.on("polling", (data)=>{
-        requirePlayerAndRoom(socket, ({player:currentPlayer, room})=>{
-            if(pollingArray.size==0){
-                const alivePlayers = room.players.filter((p:player)=>!p.dead)
-                for(let p of alivePlayers){
+    socket.on("polling", (data) => {
+        requirePlayerAndRoom(socket, ({ player: currentPlayer, room }) => {
+            if (pollingArray.size == 0) {
+                const alivePlayers = room.players.filter((p: player) => !p.dead)
+                for (let p of alivePlayers) {
                     pollingArray.set(p.username, 0);
                 }
             }
-            
-            if(currentPlayer.dead){
+
+            if (currentPlayer.dead) {
                 socket.emit("error", "dead players cannot vote")
                 return
             }
 
-            //here data will be just the suspected players interface
-            if(pollingArray.has(data.username)){
-                const currentValue = pollingArray.get(data.username)! 
-                pollingArray.set(data.username, currentValue+1);
+            if (pollingArray.has(data.username)) {
+                const currentValue = pollingArray.get(data.username)!
+                pollingArray.set(data.username, currentValue + 1);
             }
 
-            io.to(currentPlayer.roomID).emit("polling-update",  Object.fromEntries(pollingArray))
+            io.to(currentPlayer.roomID).emit("polling-update", Object.fromEntries(pollingArray))
             pollingArray.clear()
+        })
+    })
+
+    socket.on("donePolling" , (socket:Socket, io:Server)=>{
+        requirePlayerAndRoom(socket, ({player:currentPlayer, room})=>{
+            const alivePlayers:number = room.players.filter((p:player)=>!p.dead).length
+            let maxVotedPlayer:string|null = null, maxVotes:number = -1;
+            for(const [username, votes] of pollingArray.entries()){
+                if(maxVotes<votes) {
+                    maxVotedPlayer = username
+                    maxVotes = votes 
+                }
+            }
+            if(maxVotes>=0.5*alivePlayers && maxVotedPlayer && room.players.has(maxVotedPlayer)){
+                const eliminatedplayer = room.players.find((p:player)=>p.username===maxVotedPlayer)
+                if(eliminatedplayer){
+                    eliminatedplayer.dead = true
+                }
+
+                io.to(room.id).emit("player-eliminated", eliminatedplayer.username)
+            }
+
+            
         })
     })
 }
